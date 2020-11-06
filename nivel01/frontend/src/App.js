d4ee198cad6325fcf53401e22ba3152738896cd0
cga <carolinagalmeida.eng@gmail.com>
2020-11-06 09:20:45 -0300

    listando-projetos-api

--- nivel01/frontend/src/App.js
+++ nivel01/frontend/src/App.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
 import Header from './components/Header';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
-function App(){
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+function App() {
+  const [projects, setProjects] = useState([]);
 
-    // useState retorna um array com 2 posições:
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  // useState retorna um array com 2 posições:
   // 1. Variável com seu valor inicial
   // 2. Função para atualizarmos esse valor
 
@@ -15,16 +22,15 @@ function App(){
     setProjects([...projects, `Novo Projeto ${Date.now()}`]);
   }
 
-    return (
+  return (
     <>
-        <Header title="projects"></Header>
-        <img width={300} src={backgroundImage} alt=""/>
-        <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+      <Header title="projects"></Header>
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
-            <button type="button" onClick={handleAddProject}></button>
+      <button  type="button" onClick={handleAddProject}>Adicionar Projeto</button>
     </>
-        )
+  )
 }
 
 export default App;
